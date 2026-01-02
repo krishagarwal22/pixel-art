cc22a14c5b34f79108f7ffc0251459462ec349a0
Krish Agarwal <krishaga@andrew.cmu.edu>
2026-01-02 02:48:22 -0800

    no CV, V1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import AugmentedFeed from "./components/AugmentedFeed";
 import { useState } from "react";
-import Slider from "@mui/material/Slider";
+import { Typography, Slider } from "@mui/material";
 
 function App() {
   /* resolution in terms of tens of characters per line */
-  const [asciiWidth, setAsciiWidth] = useState(50);
+  const [asciiWidth, setAsciiWidth] = useState(35);
   /* characters used in ascii conversion, from darkest to lightest */
   const [chars, setChars] = useState(" .:-=+*#%@");
 
@@ -14,8 +14,11 @@ function App() {
 
   return (
     <div style={{ padding: "50px" }}>
+      <Typography variant="h5" align="center" gutterBottom>
+        Adjust Resolution
+      </Typography>
       <Slider value={asciiWidth} onChange={updateWidth} />
-      <AugmentedFeed width={6 * asciiWidth + 25} chars={chars} />
+      <AugmentedFeed width={4 * asciiWidth + 25} chars={chars} />
     </div>
   );
 }
